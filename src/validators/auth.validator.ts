d3f5dc6d880ebb0a86b7dc9d03536b90099fb5c5
Aremu Oluwagbamila <aaremu@quidax.com>
2023-09-19 22:35:15 +0100

    feat: add validation to necessary auth routes

--- src/validators/auth.validator.ts
+++ src/validators/auth.validator.ts
@@ -1,15 +1,43 @@
 import { Joi } from "celebrate";
 import bodyValidator from "../utils/bodyValidator";
 
+const emailValidator = Joi.string().email({ minDomainSegments: 2 }).required();
+const passwordValidator = Joi.string()
+  .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\W_]).{8,}$/)
+  .message(
+    "Password must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long."
+  );
+
 const loginSchema = Joi.object().keys({
-  email: Joi.string().email({ minDomainSegments: 2 }).required(),
-  password: Joi.string()
-    .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\W_]).{8,}$/)
-    .message(
-      "Password must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long."
-    ),
+  email: emailValidator,
+  password: passwordValidator,
+});
+
+const signupSchema = Joi.object().keys({
+  organization_name: Joi.string().required().min(5),
+  email: emailValidator,
+  password: passwordValidator,
+  first_name: Joi.string().required().min(3),
+  last_name: Joi.string().required().min(3),
+});
+
+const systemAccountSignupSchema = Joi.object().keys({
+  password: passwordValidator,
+  email: emailValidator,
+  first_name: Joi.string().required().min(3),
+  last_name: Joi.string().required().min(3),
+});
+
+const emailValidationSchema = Joi.object().keys({
+  email: emailValidator,
 });
 
 export const authValidator = {
   login: bodyValidator(loginSchema),
+  signup: bodyValidator(signupSchema),
+  systemAccountSignup: bodyValidator(systemAccountSignupSchema),
+  resetPassword: bodyValidator(emailValidationSchema),
+  forgotPassword: bodyValidator(emailValidationSchema),
+  sendEmailVerification: bodyValidator(emailValidationSchema),
+  changePassword: bodyValidator(emailValidationSchema),
 };
