753cf74cd9edf3c0a79295882bcd94a985699f40
Inioluwa <57904303+innextinit@users.noreply.github.com>
2023-12-04 22:32:51 +0100

    Update app.ts
    update whitelist

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const { NODE_ENV } = config;
 
 const app = express();
 
-let whitelist: string[] = ["https://ascend.africa", "https://staging.ascend.africa"];
+let whitelist: string[] = ["https://ascend.africa", "https://www.ascend.africa", "ascend.africa", "https://staging.ascend.africa"];
 
 if (NODE_ENV !== "production") {
   whitelist = [...whitelist, "http://localhost:3000", "http://127.0.0.1:3000", "https://school-management-git-staging-ascendhq.vercel.app", "https://ascend-africa.vercel.app"];
